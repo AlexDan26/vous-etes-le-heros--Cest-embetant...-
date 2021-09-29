1d340bed7733b85b7478cc4eb96d1fcc05896197
AlexDan26 <90867270+AlexDan26@users.noreply.github.com>
2021-09-29 16:24:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 console.log("allo");
-function chap1 () {
+
+let chap1 = {
+ subtitle : "Sed ut perspiciatis"'
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",«
+  img: ,
   
-}
+};
+
+let chapterObj = {
+  objet: "porte-voix",
+  chap1: "",
+  chap2: "",
+  chap3: ""
+};
