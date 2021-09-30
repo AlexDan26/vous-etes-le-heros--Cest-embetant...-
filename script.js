273512b8790fb1dd166182d88950f12ab0ea1962
AlexDan26 <90867270+AlexDan26@users.noreply.github.com>
2021-09-30 13:10:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-console.log("allo");
-
 let chap1 = {
- subtitle : "À la plage, il y a... le début de l'histoire",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",«
-  img: ,
-  
+ subtitle : "La face non cachée de la lune",
+  text: "Vous vous retrouvez dans une simulation un peu... Bizarre pour sauver votre compagnon qui y est enfermé. Devant vous, il y a un paysage de cosmos. Que faites-vous?",
+  img: "assets/Space.PNG"
 };
 
 let chap2 = {
- subtitle : "Sed ut perspiciatis"'
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",«
-  img: ,
+ subtitle : "«Je déteste le sable.»",
+  text: "L'étoile vous téléporte sur une île déserte. Il n'y a que du bois et quelque pièces électronique. Que faire avec cela?",
+  img: "assets/Beach.PNG"
   
 };
 
 let chap3 = {
- subtitle : "Sed ut perspiciatis"'
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",«
-  img: ,
+ subtitle : "Le combat décisif",
+  text: "Une fois rendu plutôt loin de l'Île, vous vous téléporter encore, dans une foule autour d'un ring de boxe. Sur le ring, se trouve votre compagnon face à un grand luchadore.",
+  img: "assets/Fight.PNG"
   
 };
 
-let premiererChoix = {
- subtitle: 
- options: //Il faut que ce soit un tableau, chaque actions doit être un objet et avoir un texte et une actions
+let premiererChoixChap1 = {
+ options: options_choix_1 = [rester_lune, vers_toile_brillante]
+}
+
+let premiererChoixChap2 = {
+  options: options_choix_2 = [faire_radio, faire_porte_voix, faire_radeau]
+}
+
+ let premiererChoixChap3 = {
+  options: options_choix_3 = [crier_frapper]
+}
+
+rester_lune = {
+  text: "...Mouais... C'est un peu ennuyant l'espace..."
+}
+
+vers_toile_brillante = {
+  text: "Vous vous dirigez vers l'étoile brillante."
+}
+
+faire_radio = {
+  text: "Bonne idée pour tenter de contacter votre ami, mais il n'y a pas d'électricité sur une île déserte... idiot."
+}
+
+faire_porte_voix = {
+  text: "Vous fabriquez un porte-voix. Il ne va peut-être pas vous ervir à grand chose, mais il est cool, donc vous le gardez. Vous avez obtenu le Porte-voix!"
+}
+
+faire_radeau = {
+  text: "Vous fabriquez le radeau avec le pièce de bois qu'il se trouve sur l'île... Vous naviguez vers l'horizon..."
+}
+
+crier_frapper = {
+  text: "Vous crier à votre ami des conseils pour battre le luchadore."
 }
 
 let chapterObj = {
